5c198174ebd36f8514ea0900a77b93ed0e757fa4
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-20 12:30:40 +0100

    checkout component added, paying buttos in cart connected to checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   { path:"home", component: HomeComponent},
   { path:"cart", component: CartComponent},
+  { path:"checkout", component: CheckoutComponent},
   { path:"", component:HomeComponent },
   { path:"**", component:HomeComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { AdvertComponent } from './advert/advert.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CartComponent } from './cart/cart.component';
     HomeComponent,
     AdvertComponent,
     ProductListComponent,
-    CartComponent
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
