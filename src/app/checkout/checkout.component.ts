6b5321375a7f31c7ce22c20fe291b1920681bf83
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-23 11:58:34 +0100

    checkout compontent without functionality

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class CheckoutComponent {
 
+  cart = [
+    {"termek_id":"52","nev":"Luxury","mennyiseg":1,"uj_ar":9995},
+    {"termek_id":"64","nev":"Opal","mennyiseg":3,"uj_ar":4995,},
+    {"termek_id":"73","nev":"Flower","mennyiseg":2,"uj_ar":14385},
+    {"termek_id":"77","nev":"Hedera","mennyiseg":1,"uj_ar":7995},
+    {"termek_id":"52","nev":"Luxury","mennyiseg":11,"uj_ar":9995}
+  ]
+
+  entity:String = "person"
+  deliveryOpt:String = "house"
+
+
+
 }
