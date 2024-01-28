a10268c905b7edf43f12edc209d1b5222f88b6ce
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-28 17:34:56 +0100

    now if you click on a product in the productList you can see the product component with the selected product; layout changes in product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -6,25 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  product={
-    "termek_id":49,
-    "kategoria_id":3,
-    "nev":"B52",
-    "db":78,
-    "regi_ar":0,
-    "uj_ar":5095,
-    "ar_mod_datum":"2023-10-17",
-    "lathato":"1",
-    "anyag":"100% Poliészter",
-    "mintazat":"Egyszínű",
-    "meret":"80 cm x 150 cm",
-    "szin":"multi",
-    "szalmagassag":"alacsony",
-    "vastagsag":"7 mm",
-    "stilus":"modern"
-  }
+
+  product: any;
 
   categories= ["üres", "Padlószőnyeg", "Szőnyeg", "Futószőnyeg", "Lábtörlő"]
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.product = params;
+    });
+  }
 
 
 }
