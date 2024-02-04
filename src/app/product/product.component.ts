a6779a7dcfc65804acb2c37b58aa59310b8e6e84
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-02-04 14:43:10 +0100

    ratings implemented in product component, need to change the design

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,8 +10,38 @@ export class ProductComponent {
 
   product: any;
 
+  ratings = [
+    {
+      user: "Mónika",
+      score: 5,
+      text: "Nagyon jó mínőségű termék",
+      date: "2024-01-11"
+    },
+    {
+      user: "Dezső",
+      score: 3,
+      text: "A színeket nem adja vissza megfelelően a feltöltött kép",
+      date: "2024-01-21"
+    },
+    {
+      user: "József",
+      score: 4,
+      text: "A minőség megfelelő, viszont az ár lehetne kevesebb..",
+      date: "2024-01-21"
+    },
+    {
+      user: "Béla",
+      score: 5,
+      text: "",
+      date: "2024-01-25"
+    }
+  ];
+  totalScore:number;
   categories= ["üres", "Padlószőnyeg", "Szőnyeg", "Futószőnyeg", "Lábtörlő"]
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    this.totalScore=this.sumScores()
+    console.log(this.totalScore)
+  }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
@@ -19,5 +49,14 @@ export class ProductComponent {
     });
   }
 
+  sumScores(){
+    let score = 0.0;
+    let qty = 0;
 
+    this.ratings.forEach(rating => {
+      score = score + rating.score
+      qty++;
+    });
+    return score/qty
+  }
 }
