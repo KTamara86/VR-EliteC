9d87c1c9bcb6415ccb2f3b52b6a28f4612b29e35
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-03-21 19:32:01 +0100

    function: able to reach products in db

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,10 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
 
+  url = "https://elitecarpets-2fa53-default-rtdb.europe-west1.firebasedatabase.app"
+  productsSub = new Subject()
+
+  constructor(private db:AngularFireDatabase, private http:HttpClient){
+    this.loadProducts()
+  }
+
+  getProducts(){
+    return this.productsSub
+  }
+
+  private loadProducts(){
+    this.http.get(this.url + "/termekek.json").subscribe(
+      (res) => this.productsSub.next(res)
+    )
+  }
 }
