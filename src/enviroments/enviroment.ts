e2936295c8d9b64321dc645b10cb7ffd2cae2927
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-03-21 19:14:48 +0100

    adding databaseURL

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -3,6 +3,7 @@ export const environment = {
     firebase : {
         apiKey: "AIzaSyDEfhxOnmXr4Z8bvK5S7KMpABzfkrlaFiY",
         authDomain: "elitecarpets-2fa53.firebaseapp.com",
+        databaseURL: "https://elitecarpets-2fa53-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "elitecarpets-2fa53",
         storageBucket: "elitecarpets-2fa53.appspot.com",
         messagingSenderId: "586805530530",
