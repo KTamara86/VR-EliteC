a2ca675e2e670dfcf05488033e811fc30672a216
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-03-23 19:32:31 +0100

    cart component now renders products from the cart, but! cartTotal not working

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,27 +8,23 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
 
-  cart = [
-    {"termek_id":"52","nev":"Luxury","mennyiseg":1,"uj_ar":9995},
-    {"termek_id":"64","nev":"Opal","mennyiseg":3,"uj_ar":4995,},
-    {"termek_id":"73","nev":"Flower","mennyiseg":2,"uj_ar":14385},
-    {"termek_id":"77","nev":"Hedera","mennyiseg":1,"uj_ar":7995},
-    {"termek_id":"52","nev":"Luxury","mennyiseg":11,"uj_ar":9995}
-  ]
 
-  cart2:any
+
+  cart:any
 
   constructor(private cartService: CartService){
     this.cartService.getCart().subscribe(
-      (res) => this.cart2=res
+      (res) => this.cart=res
     )
   }
 
-  cartTotal(){
-    let total = 0
-    this.cart.forEach(product => {
-      total += product.mennyiseg*product.uj_ar
-    });
-    return total
-  }
+  // cartTotal(){
+  //   let total = 0
+  //   this.cart.forEach(product => {
+  //     total += product.mennyiseg*product.uj_ar
+  //   });
+  //   return total
+  // }
+
+  //TODO: cartTotal not working
 }
