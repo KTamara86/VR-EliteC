75cd5b608c0128c48f662f7d479d56c4264eb83a
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-03-26 13:57:30 +0100

    removed cart route and unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { ProductComponent } from './product/product.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
-import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path:"home", component: HomeComponent},
-  { path:"cart", component: CartComponent},
   { path:"checkout", component: CheckoutComponent},
   { path: "userlogin", component: UserLoginComponent},
   { path: "usersignup", component: UserSignUpComponent},
