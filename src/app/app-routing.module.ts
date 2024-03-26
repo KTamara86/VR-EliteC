08ca21a106fdf0cf378bc960f57be79b11bece62
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-03-26 12:53:16 +0100

    cart route deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { CheckoutComponent } from './checkout/checkout.component';
@@ -11,7 +10,6 @@ import { VerifyEmailComponent } from './verify-email/verify-email.component';
 
 const routes: Routes = [
   { path:"home", component: HomeComponent},
-  { path:"cart", component: CartComponent},
   { path:"checkout", component: CheckoutComponent},
   { path: "userlogin", component: UserLoginComponent},
   { path: "usersignup", component: UserSignUpComponent},
