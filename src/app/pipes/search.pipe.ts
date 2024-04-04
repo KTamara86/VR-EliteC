1cd2ea099fb99acc1972a954f666ac5d22a143fa
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-04 21:50:17 +0200

    search bug fixed

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -7,14 +7,10 @@ export class SearchPipe implements PipeTransform {
 
   transform(products: any, expression: any, category: number) {
     if (!products) return null;
-    if (!expression || !category) return products;
+    if (!expression) return products;
 
-    let data = products.filter(
+    return products.filter(
       (product: any) => product.nev.toLowerCase().includes(expression.toLowerCase()));
-
-    return data.filter(
-      (product: any) => (product.nev.toLowerCase().includes(expression.toLowerCase()))
-    );
   }
 
 }
