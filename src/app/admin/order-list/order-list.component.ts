22295679b2836513ed6546f091b35165be65012e
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-08 19:40:34 +0200

    modifyOrder start

--- src/app/admin/order-list/order-list.component.ts
+++ src/app/admin/order-list/order-list.component.ts
@@ -28,26 +28,25 @@ export class OrderListComponent {
     this.orderService.reload()
   }
 
-  modifyRating(order:any){
-    // let text = "CENZÚRÁZOTT VÉLEMÉNY: A vélemény kívül esett a jó ízlés határain"
-    let body = {
-      userid: this.user.userid, 
-      consignee: this.data.consignee,
-      taxnumber: this.data.taxnumber,
-      phone: this.data.phone,
-      zipcode: this.data.zipcode,
-      city: this.data.city,
-      address: this.data.address,
-      deliveryZipcode: this.data.deliveryZipcode,
-      deliveryCity: this.data.deliveryCity,
-      deliveryAddress: this.data.deliveryAddress,
-      datetime: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
-      status:"megrendelve",
-      qty: this.totalQty,
-      total: this.total,
-      packetPoint: false,
-      payment: this.paymentOpt
-    }
-    this.orderService.writeOrderData(body, order.key)
-  }
+  // modifyOrder(order:any){
+  //   let body = {
+  //     userid: this.user.userid, 
+  //     consignee: this.data.consignee,
+  //     taxnumber: this.data.taxnumber,
+  //     phone: this.data.phone,
+  //     zipcode: this.data.zipcode,
+  //     city: this.data.city,
+  //     address: this.data.address,
+  //     deliveryZipcode: this.data.deliveryZipcode,
+  //     deliveryCity: this.data.deliveryCity,
+  //     deliveryAddress: this.data.deliveryAddress,
+  //     datetime: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+  //     status:"megrendelve",
+  //     qty: this.totalQty,
+  //     total: this.total,
+  //     packetPoint: false,
+  //     payment: this.paymentOpt
+  //   }
+  //   this.orderService.writeOrderData(body, order.key)
+  // }
 }
