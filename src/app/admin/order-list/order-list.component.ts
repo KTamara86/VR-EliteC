d8470aaa5a032e16c13e7443117af471ed1f7ccf
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-08 20:12:08 +0200

    data binding

--- src/app/admin/order-list/order-list.component.ts
+++ src/app/admin/order-list/order-list.component.ts
@@ -28,25 +28,10 @@ export class OrderListComponent {
     this.orderService.reload()
   }
 
-  // modifyOrder(order:any){
-  //   let body = {
-  //     userid: this.user.userid, 
-  //     consignee: this.data.consignee,
-  //     taxnumber: this.data.taxnumber,
-  //     phone: this.data.phone,
-  //     zipcode: this.data.zipcode,
-  //     city: this.data.city,
-  //     address: this.data.address,
-  //     deliveryZipcode: this.data.deliveryZipcode,
-  //     deliveryCity: this.data.deliveryCity,
-  //     deliveryAddress: this.data.deliveryAddress,
-  //     datetime: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
-  //     status:"megrendelve",
-  //     qty: this.totalQty,
-  //     total: this.total,
-  //     packetPoint: false,
-  //     payment: this.paymentOpt
-  //   }
-  //   this.orderService.writeOrderData(body, order.key)
-  // }
+modifyOrder(order:any){
+  
+  console.log(order)
+
+  // this.orderService.writeOrderData(body, order.key)
+  }
 }
