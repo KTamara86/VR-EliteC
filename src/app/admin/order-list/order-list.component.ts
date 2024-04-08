42cb27df9e66195d7e320f09305c7b26e870e933
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-08 17:52:31 +0200

    order-list started

--- src/app/admin/order-list/order-list.component.ts
+++ src/app/admin/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-order-list',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent {
+  
+  ordersArray:any
 
+  constructor(private orderService:OrderService){
+    orderService.getOrders().subscribe(
+      (res:any) => {
+        let array = []
+        for(const key in res){
+          let element = res[key]
+          element.key = key
+          array.push(element)
+        }
+        this.ordersArray = array 
+      }
+    )
+  }
+
+  ngOnInit(){
+    this.orderService.reload()
+  }
+
+  modifyRating(order:any){
+    // let text = "CENZÚRÁZOTT VÉLEMÉNY: A vélemény kívül esett a jó ízlés határain"
+    let body = {
+    //   product: rating.product,
+    //   rating: rating.rating,
+    //   text: text,
+    //   time: rating.time,
+    //   user: rating.user
+    }
+    this.orderService.writeOrderData(body, order.key)
+  }
 }
