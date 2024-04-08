87e83fc68cef909ac9712b359b85473bc859bf42
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-08 16:22:48 +0200

    basic layout and function

--- src/app/admin/rating-list/rating-list.component.ts
+++ src/app/admin/rating-list/rating-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RatingService } from 'src/app/services/rating.service';
 
 @Component({
   selector: 'app-rating-list',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./rating-list.component.css']
 })
 export class RatingListComponent {
+
+  ratingsArray:any
+
+  constructor(private ratingService:RatingService){
+    ratingService.getRatings().subscribe(
+      (res:any) => {
+        let array = []
+        for(const key in res){
+          array.push(res[key])
+        }
+        this.ratingsArray = array 
+      }
+    )
+  }
+
+  ngOnInit(){
+    this.ratingService.reload()
+  }
+
+  modifyRating(){
+    let text = "CENZÚRÁZOTT VÉLEMÉNY: A vélemény kívül esett a jó ízlés határain"
+    console.log(text)
+  }
 
 }
