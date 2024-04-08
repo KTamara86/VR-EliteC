24c248237d3e3569437089794be22d72d51d45d3
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-09 01:47:04 +0200

    product-list started: css added, in html product creating section renders data inputs or selects of form

--- src/app/admin/product-list/product-list.component.ts
+++ src/app/admin/product-list/product-list.component.ts
@@ -7,4 +7,71 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  searchArgs = ""
+
+  newProduct = {
+    key: "",
+    anyag: "",
+    db: "",
+    lathato: "",
+    meret: "",
+    mintazat: "",
+    nev: "",
+    stilus: "",
+    szalmagassag: "",
+    szin: "",
+    uj_ar: "",
+    vastagsag: ""
+  }
+
+  props = {
+    anyag: ["100% Poliészter", "100% polipropilén", "100% polyester", "100% Polypropilen", "100% PP Heatset"],
+    mintazat: ["Egyszínű", "mintás"],
+    meret: ["80 cm x 150 cm", "200 cm x 290 cm", "160 cm x 230 cm", "120 cm x 170 cm", "40 cm x 60 cm", "60 cm x 110 cm"],
+    szin: ["barna", "beige", "fehér", "fekete", "kék", "krém", "lila", "multi", "piros", "rózsaszín", "szürke", "terra", "zöld"],
+    szalmagassag: ["alacsony", "közepes", "magas"],
+    vastagsag: ["7 mm", "12 mm", "14 mm"],
+    stilus: ["modern", "klasszikus"]
+  }
+
+  // props = [
+  //   {
+  //     "key": "anyag",
+  //     "name": "Anyag",
+  //     "options":  ["100% Poliészter", "100% polipropilén", "100% polyester", "100% Polypropilen", "100% PP Heatset"]
+  //   },
+  //   {
+  //     "key": "mintazat",
+  //     "name": "Mintázat",
+  //     "options":  ["Egyszínű", "mintás"]
+  //   },
+  //   {
+  //     "key": "meret",
+  //     "name": "Méret",
+  //     "options":  ["80 cm x 150 cm", "200 cm x 290 cm", "160 cm x 230 cm", "120 cm x 170 cm", "40 cm x 60 cm", "60 cm x 110 cm"]
+  //   },
+  //   {
+  //     "key": "szin",
+  //     "name": "Szín",
+  //     "options":  ["barna", "beige", "fehér", "fekete", "kék", "krém", "lila", "multi", "piros", "rózsaszín", "szürke", "terra", "zöld"]
+  //   },
+  //   {
+  //     "key": "szalmagassag",
+  //     "name": "Szálmagasság",
+  //     "options":  ["alacsony", "közepes", "magas"]
+  //   },
+  //   {
+  //     "key": "vastagsag",
+  //     "name": "Vastagság",
+  //     "options":  ["7 mm", "12 mm", "14 mm"]
+  //   },
+  //   {
+  //     "key": "stilus",
+  //     "name": "Stílus",
+  //     "options":  ["modern", "klasszikus"]
+  //   }]
+
+  postProduct(){
+    //funkciót kiépítése
+  }
 }
