b788f77433dd1c0866952f36fc20a8e20c3c0351
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-09 02:23:27 +0200

    notes created

--- src/app/admin/product-list/product-list.component.ts
+++ src/app/admin/product-list/product-list.component.ts
@@ -33,45 +33,20 @@ export class ProductListComponent {
     vastagsag: ["7 mm", "12 mm", "14 mm"],
     stilus: ["modern", "klasszikus"]
   }
-
-  // props = [
-  //   {
-  //     "key": "anyag",
-  //     "name": "Anyag",
-  //     "options":  ["100% Poliészter", "100% polipropilén", "100% polyester", "100% Polypropilen", "100% PP Heatset"]
-  //   },
-  //   {
-  //     "key": "mintazat",
-  //     "name": "Mintázat",
-  //     "options":  ["Egyszínű", "mintás"]
-  //   },
-  //   {
-  //     "key": "meret",
-  //     "name": "Méret",
-  //     "options":  ["80 cm x 150 cm", "200 cm x 290 cm", "160 cm x 230 cm", "120 cm x 170 cm", "40 cm x 60 cm", "60 cm x 110 cm"]
-  //   },
-  //   {
-  //     "key": "szin",
-  //     "name": "Szín",
-  //     "options":  ["barna", "beige", "fehér", "fekete", "kék", "krém", "lila", "multi", "piros", "rózsaszín", "szürke", "terra", "zöld"]
-  //   },
-  //   {
-  //     "key": "szalmagassag",
-  //     "name": "Szálmagasság",
-  //     "options":  ["alacsony", "közepes", "magas"]
-  //   },
-  //   {
-  //     "key": "vastagsag",
-  //     "name": "Vastagság",
-  //     "options":  ["7 mm", "12 mm", "14 mm"]
-  //   },
-  //   {
-  //     "key": "stilus",
-  //     "name": "Stílus",
-  //     "options":  ["modern", "klasszikus"]
-  //   }]
-
+  
   postProduct(){
-    //funkciót kiépítése
+    //TODO: funkciót kiépítése
+  }
+
+  deleteProduct(){
+    //TODO: funkciót kiépítése
+  }
+
+  modifyProduct(){
+    //TODO: funkciót kiépítése
+  }
+
+  toastMsgOutlet(){
+    //TODO: funkciót kiépíteni
   }
 }
