b2137217e910340cae47005b264d042d5a057a42
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-04-16 22:06:26 +0200

    modifying method is working, but there is a bug in the service: multiples data in his storage

--- src/app/components/profil/ratings-data/ratings-data.component.ts
+++ src/app/components/profil/ratings-data/ratings-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 import { RatingService } from 'src/app/services/rating.service';
 
@@ -11,7 +12,7 @@ export class RatingsDataComponent implements OnInit {
   
     myRatings:any
   
-  constructor(private auth:AuthService, private rs:RatingService) { }
+  constructor(private auth:AuthService, private rs:RatingService, private toastr:ToastrService) { }
   
   ngOnInit(): void {
     this.myRatings = this.rs.loadMyRatings(this.auth.user.email)
@@ -22,6 +23,24 @@ export class RatingsDataComponent implements OnInit {
   }
   
   modify(rating:any){
+    let key = rating.key
+    rating.key = null
+    this.rs.writeRatingData(rating, key).then(
+      (res) => this.toastMsg(res)
+    )
+  }
 
+  toastMsg(result:boolean){
+    let props:any = {
+      closeButton: true,
+      timeOut: 2000,
+      progressBar: true,
+      progressAnimation: "decreasing",
+      positionClass: "toast-top-right",
+      newestOnTop: true
+    }
+
+    if(result) this.toastr.info("Sikeresen módostottad a véleményt", "SIKER", props)
+    else this.toastr.warning("Valami hiba lépett fel, próbálkozz később", "HIBA", props)
   }
 }
